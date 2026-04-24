71dcde5861ef6f1dc4a9531e1656ebcff9c87bb0
Sandra Gajic <66118122+sgajic369@users.noreply.github.com>
2026-04-24 17:53:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,28 @@ def draw():
     img.draw()
 
 def place_zoey():
+    img.image = "explosion"
+    
+    clock.schedule_unique(reset_image, 0.2)
+
+def reset_image():
+    img.image = "zoeylol"
+    curr_width, curr_height = img.width, img.height
+    if curr_width > 60:
+        new_size = (int(curr_width * 0.9), int(curr_height * 0.9))
+        img._surf = pygame.transform.scale(img._surf, new_size)
+        img._update_pos()
+
+    img.x = randint(100, WIDTH - 100)
+    img.y = randint(100, HEIGHT - 100)
+    animate(img, tween='bounce_end', duration=0.5, y=img.y + 60)
+
+def place_zoey():
     curr_width = img.width
     curr_height = img.height
 
     # Shrink img by 20% 
-    if curr_width > 100 and curr_height > 100:
+    if curr_width > 60 and curr_height > 60:
         new_width = int(curr_width * 0.9)
         new_height = int(curr_height * 0.9)
         # 
@@ -31,6 +48,7 @@ def place_zoey():
 
 def on_mouse_down(pos):
     if img.collidepoint(pos):
+        sounds.eep.play()
         print("Good shot!") 
         place_zoey()
     else:
