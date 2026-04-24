24d8ddb72cd893cf52062824097b6ca786f61522
Sandra Gajic <66118122+sgajic369@users.noreply.github.com>
2026-04-24 17:35:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import randint
-import pgzrun  # Needed to start the game from the code
+import pgzrun
+import pygame
 
 WIDTH = 1920
 HEIGHT = 1080
@@ -11,10 +12,22 @@ def draw():
     img.draw()
 
 def place_zoey():
+    curr_width = img.width
+    curr_height = img.height
+
+    # Shrink img by 20% 
+    if curr_width > 100 and curr_height > 100:
+        new_width = int(curr_width * 0.9)
+        new_height = int(curr_height * 0.9)
+        # 
+        img._surf = pygame.transform.scale(img._surf, (new_width, new_height))
+        img._update_pos() 
+    # The img is 1720x880
     img.x = randint(100, WIDTH - 200)
     img.y = randint(100, HEIGHT - 200)
     
-    animate(img, tween='bounce_end', duration=0.5, y=img.y + 60)
+    # Vibrating effect
+    animate(img, tween='bounce_end', duration=0.34555, y=img.y + 60)
 
 def on_mouse_down(pos):
     if img.collidepoint(pos):
@@ -25,7 +38,6 @@ def on_mouse_down(pos):
         quit()
 
 place_zoey()
-
 
 try:
     pgzrun.go()
